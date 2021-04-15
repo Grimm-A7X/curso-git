14ce404a270fb00eab01235ea2badf0d019b2ee3
Christian Souza <christianyugioh@gmail.com>
2021-04-15 19:20:27 -0300

    Acrescentada pergunta como você está hoje?

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -8,6 +8,9 @@ namespace projeto_teste
         {
             Console.WriteLine("Olá Mundo!");
             Console.WriteLine("Bom dia");
+            Console.WriteLine("Como você está hoje?");
+
+            Console.ReadLine();
         }
     }
 }
